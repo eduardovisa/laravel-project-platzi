d54dd48237211e22f360c06300bc406a10bd09cd
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-29 09:03:21 -0600

    Request dashboard->index created

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
+
         return view('test', [
-            'title' => 'Platzi Laravel Course!!'
+            'title' => $request -> query(key: 'title', default: 'Default value')
         ]);
     }
 }
